36d425233be74869b628906cf0aea1b77016fbfe
wonwoo <aoruqjfu@gmail.com>
2018-07-09 19:49:13 +0900

    Security 추가

---
+++ src/main/java/ml/wonwoo/zookeepermanager/config/WebSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package ml.wonwoo.zookeepermanager.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .formLogin()
+        .loginPage("/login")
+        .permitAll()
+        .and()
+        .logout().permitAll();
+  }
+
+}
+
