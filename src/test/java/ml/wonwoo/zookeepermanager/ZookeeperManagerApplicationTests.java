2666b83ecf61498ba4dc112bf7ab02ff32dd0e7b
wonwoo <aoruqjfu@gmail.com>
2018-07-12 21:35:22 +0900

    test 수정

--- src/test/java/ml/wonwoo/zookeepermanager/ZookeeperManagerApplicationTests.java
+++ src/test/java/ml/wonwoo/zookeepermanager/ZookeeperManagerApplicationTests.java
@@ -1,18 +1,11 @@
 package ml.wonwoo.zookeepermanager;
 
-import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.retry.RetryOneTime;
 import org.apache.curator.test.TestingServer;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
@@ -23,14 +16,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 public class ZookeeperManagerApplicationTests {
 
-  @Autowired
-  private TestingServer zkTestServer;
+  private static TestingServer zkTestServer;
 
-  private CuratorFramework curatorFramework;
-
-  @Before
-  public void setup()  {
-    curatorFramework = CuratorFrameworkFactory.newClient(zkTestServer.getConnectString(), new RetryOneTime(2000));
+  @BeforeClass
+  public static void setup() throws Exception {
+    zkTestServer = new TestingServer(2181);
   }
 
   @Test
@@ -38,19 +28,8 @@ public class ZookeeperManagerApplicationTests {
     assertThat(zkTestServer).isNotNull();
   }
 
-  @After
-  public void stop() throws IOException {
+  @AfterClass
+  public static void stop() throws IOException {
     zkTestServer.stop();
-    curatorFramework.close();
   }
-
-  @Configuration
-  public static class ZooConfig {
-
-    @Bean
-    TestingServer zkTestServer() throws Exception {
-      return new TestingServer(2181);
-    }
-  }
-
 }
